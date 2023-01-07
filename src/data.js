3ea4521b52f1420811d6858fb8d72db480444363
rishitdaru <rdaru@uwaterloo.ca>
2023-01-07 11:18:27 -0500

    adding-src-files

---
+++ src/data.js
@@ -0,0 +1,60 @@
+const data = [
+    {
+        id: 1,
+        title: "Solo Mountain Biking",
+        description:
+          "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+          rating: 4.7,
+          reviewCount: 5,
+        },
+        location: "Norway",
+        openSpots: 10,
+    },
+    {
+      id: 2,
+      title: "Learn Canyon Photography",
+      description:
+        "Interested in becoming a nature photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the wandarlust travellers a memory of a lifetime.",
+      price: 125,
+      coverImg: "grand-canyon.png",
+      stats: {
+        rating: 5.0,
+        reviewCount: 34,
+      },
+      location: "USA",
+      openSpots: 0,
+    },
+    {
+      id: 3,
+      title: "Diving in The Great Barrier Reef",
+      description:
+        "Experience the serene and beautiful sealife just North of Sydney. 1 hour of tranquilness!",
+      price: 580,
+      coverImg: "scuba-diving.png",
+      stats: {
+        rating: 4.8,
+        reviewCount: 96,
+      },
+      location: "Australia",
+      openSpots: 16,
+    },
+    {
+        id: 4,
+        title: "Work Ethics with Ankur Warikoo",
+        description:
+          'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
+        price: 100,
+        coverImg: "ankur-warikoo.png",
+        stats: {
+          rating: 4.9,
+          reviewCount: 65,
+        },
+        location: "Online",
+        openSpots: 9,
+      }
+  ];
+  
+  export default data;
